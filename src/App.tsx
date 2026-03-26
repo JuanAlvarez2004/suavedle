718191eb570a4a885917c34ed04378ca2f2de3de
JuanAlvarez2004 <168690900+JuanAlvarez2004@users.noreply.github.com>
2026-03-26 16:47:06 -0500

    feat: set up fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 function App() {
   return (
-    <h1 className="text-9xl font-bold">SUAVEDLE</h1>
+    <div className="m-8">
+      <h1 className="text-9xl font-bold font-array-bold">SUAVEDLE</h1>
+      <p className="font-dot-gothic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi a repellat suscipit veniam consequatur! Sapiente illum quia, rerum odit inventore id, ex, excepturi quae illo mollitia aspernatur accusantium iusto. Ex.</p>
+    </div>
   )
 }
 
